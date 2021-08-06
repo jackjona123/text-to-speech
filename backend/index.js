1146fd02d882de9927b3be7cfd627613218babb2
jackjona123 <65679392+jackjona123@users.noreply.github.com>
2021-08-05 23:08:40 -0400

    test

--- backend/index.js
+++ backend/index.js
@@ -1,61 +1,60 @@
-exports.handler = function index(event, context, callback) {
-    // Import request module
-    const request = require('request')
-    
-    /**
-     * Responds to any HTTP request.
-     *
-     * @param {!express:Request} req HTTP request context.
-     * @param {!express:Response} res HTTP response context.
-     */
-    
-    exports.tts = (req, res) => {
-        // Enable CORS
-        res.set('Access-Control-Allow-Origin', '*')
-        res.set('Access-Control-Allow-Methods', 'GET, POST')
-        res.set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
-    
-        // Intercept non-POST requests
-        if (req.method != 'POST') {
-            res.send('The Service Is Running :)')
-            return
-        }
-    
-        // Prepare request params for Streamlabs
-        let voice = req.body.voice || 'Brian'
-        let text = req.body.text || 'Please select a voice and message.'
-    
-        // Send request to Streamlabs' API server
-        request.post(
-            'https://streamlabs.com/polly/speak', {
-                json: {
-                    voice: voice,
-                    text: text
-                }
-            },
-            function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    res.status(200).json(body);
-                } else {
-                    res.status(500).json({
-                        success: false,
-                        reason: 'Streamlabs API error.',
-                        error: error,
-                        code: response.statusCode
-                    })
-                }
-            }
-        )
-    
-        // Log output to console
-        const jsonLog = {
-            payload: {
+// Import request module
+const request = require('request')
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+
+exports.tts = (req, res) => {
+    // Enable CORS
+    res.set('Access-Control-Allow-Origin', 'https://www.tts.jackjona.live')
+    res.set('Access-Control-Allow-Origin', 'https://tts.jackjona.live')
+    res.set('Access-Control-Allow-Methods', 'GET, POST')
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
+
+    // Intercept non-POST requests
+    if (req.method != 'POST') {
+        res.send('ok')
+        return
+    }
+
+    // Prepare request params for Streamlabs
+    let voice = req.body.voice || 'Brian'
+    let text = req.body.text || 'Please select a voice and message.'
+
+    // Send request to Streamlabs' API server
+    request.post(
+        'https://streamlabs.com/polly/speak', {
+            json: {
                 voice: voice,
                 text: text
-            },
-            requestIP: req.ip
+            }
+        },
+        function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.status(200).json(body);
+            } else {
+                res.status(500).json({
+                    success: false,
+                    reason: 'Streamlabs API error.',
+                    error: error,
+                    code: response.statusCode
+                })
+            }
         }
-        console.log(`${voice}: ${text}`)
-        console.debug(jsonLog)
+    )
+
+    // Log output to console
+    const jsonLog = {
+        payload: {
+            voice: voice,
+            text: text
+        },
+        requestIP: req.ip
     }
+    console.log(`${voice}: ${text}`)
+    console.debug(jsonLog)
 }
